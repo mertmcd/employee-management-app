b745fbb93a1e064ac7c2a932868afc277ae86e7a
Mert Demir <mert.demir@decoverse.com>
2024-11-09 17:48:48 +0300

    feat: add localization support for Turkish, including locale codes and translation files

---
+++ src/generated/locales/tr.js
@@ -0,0 +1,54 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    import {html} from 'lit';
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      'h7d09afc329d9995b': html`Sayfa ${0} / ${1}`,
+'s0fbf6dc6a1966408': `Sonraki`,
+'s138cd2d972d2bb50': `Soyisim en az 2 karakter olmalıdır`,
+'s193dbf11cd670898': `Çalışanı Kaydet`,
+'s1e45a4d7635fbda7': `Departman`,
+'s2024c0cf01389edb': `Lütfen geçerli bir doğum tarihi girin`,
+'s24aadbfb1b1a1a11': `Teknik`,
+'s2714784162ec459d': `Devam`,
+'s2c2b04f2561a1d4b': `Analitik`,
+'s2c57913ffd36d298': `İsim`,
+'s2ceb11be2290bb1b': `İptal`,
+'s32c9b3dd030fc24c': `Çalışan Ekle`,
+'s3db6c3999daeecdb': `Çalışan Listesi`,
+'s5024b9226ba6037b': `Silmek istediğinizden emin misiniz?`,
+'s53802e3f37d072a7': `Doğum Tarihi`,
+'s64ef2a6c2dd1d3d1': `Düzenle`,
+'s666dc7ef6e9b24b0': `Lütfen geçerli bir işe giriş tarihi girin`,
+'s6a0c25d3b5bedd47': `Çalışan başarıyla kaydedildildi`,
+'s72531189c8173b33': `Tablo Görünümüne Geç`,
+'s7404d9208a486318': `Junior`,
+'s772528836550622c': `İşe Giriş Tarihi`,
+'s777c5b5e4771e42e': `Düzenlemek istediğinizden emin misiniz?`,
+'s7c3b321da7ef9637': `Lütfen geçerli bir eposta adresi girin`,
+'s8a37652cc28d3c55': `Senior`,
+'s8a9fa9684ba37683': `Çalışanı Düzenle`,
+'s8af61807443f32a4': `Aksiyonlar`,
+'s926ca092554d6219': `Çalışan başarıyla silindi`,
+'s95a75a2392af5780': `Soyisim`,
+'s985d5260b47f1ff8': `Telefon numarası 10 haneli bir numara olmalıdır`,
+'sa025c0bd1d1fea98': `İsim en az 2 karakter olmalıdır`,
+'sacc4d771f79ccced': `Medior`,
+'sbec93e4b2c54941b': `Liste Görünümüne Geç`,
+'sc1611c0558728a2a': `Pozisyon`,
+'sc1feb40abd0ba7fc': `Kayıt bulunamadı`,
+'scba00271d50c74f6': `Çalışanlar`,
+'sd1f44f1a8bc20e67': `Eposta`,
+'sd93452e245bcaf7a': `Filtrele:`,
+'sdc673e73b5c13aea': `Sil`,
+'se699118f3aa9baa1': `Kaydetmek istediğinizden emin misiniz?`,
+'sf1bc83d2509bafcf': `Telefon`,
+'sf3ff78cc329d3528': `Önceki`,
+    };
+  
